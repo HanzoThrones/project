0a043ce46d6acd20915017250cec57ee2fc43437
mfeuerstein <michael.a.feuerstein@gmail.com>
2016-06-12 02:43:51 -0400

    Third Commit

--- src/DemoGraph2.java
+++ src/DemoGraph2.java
@@ -9,7 +9,7 @@ public class DemoGraph2 {
         
         Graph graph = new Graph();
         
-        final int SIZE = 10;
+        final int SIZE = 58;
         
         //initialize some vertices and add them to the graph
         Vertex[] vertices = new Vertex[SIZE];       
@@ -25,22 +25,55 @@ public class DemoGraph2 {
         vertices[8] = new PortTile("White Harbor Port", 9, "None");
         vertices[9] = new LandTile("Widow's Watch", 10, "None");
         vertices[10] = new SeaTile("The Shivering Sea", 11, "None");
-        vertices[11] = new LandTile("Flint's Finger", 12, "None");
-        vertices[12] = new LandTile("Greywater Watch", 13, "None");
-        vertices[13] = new LandTile("Moat Calin", 14, "None");
-        vertices[14] = new SeaTile("The Narrow Sea", 15, "None");
-        vertices[15] = new SeaTile("Ironman's Bay", 16, "None");
-        vertices[16] = new LandTile("Pyke", 17, "Greyjoy");
-        vertices[17] = new PortTile("Pyke Port", 18, "Greyjoy");
-        vertices[18] = new LandTile("Seagard", 19, "None");
-        vertices[19] = new LandTile("The Twins", 20, "None");
-        vertices[20] = new LandTile("The Fingers", 21, "None");
-        vertices[21] = new LandTile("Riverrun", 22, "None");
-        vertices[22] = new LandTile("The Mountains of The Moon", 23, "None");
-        vertices[23] = new LandTile("The Eyrie", 24, "None");
-        vertices[24] = new SeaTile("The Golden Sound", 25, "None");
-        vertices[25] = new LandTile("Lannisport", 26, "None");
-        vertices[26] = new PortTile("Lannisport Port", 27, "None");
+        vertices[11] = new SeaTile("Sunset Sea", 12, "None");
+        vertices[12] = new LandTile("Flint's Finger", 13, "None");
+        vertices[13] = new LandTile("Greywater Watch", 14, "None");
+        vertices[14] = new LandTile("Moat Calin", 15, "None");
+        vertices[15] = new SeaTile("The Narrow Sea", 16, "None");
+        vertices[16] = new SeaTile("Ironman's Bay", 17, "None");
+        vertices[17] = new LandTile("Pyke", 18, "Greyjoy");
+        vertices[18] = new PortTile("Pyke Port", 19, "Greyjoy");
+        vertices[19] = new LandTile("Seagard", 20, "None");
+        vertices[20] = new LandTile("The Twins", 21, "None");
+        vertices[21] = new LandTile("The Fingers", 22, "None");
+        vertices[22] = new LandTile("Riverrun", 23, "None");
+        vertices[23] = new LandTile("The Mountains of The Moon", 24, "None");
+        vertices[24] = new LandTile("The Eyrie", 25, "None");
+        vertices[25] = new SeaTile("The Golden Sound", 26, "None");
+        vertices[26] = new LandTile("Lannisport", 27, "None");
+        vertices[27] = new PortTile("Lannisport Port", 28, "None");
+        vertices[28] = new LandTile("Stoney Sept", 29, "None");
+        vertices[29] = new LandTile("Harrenhal", 30, "None");
+        vertices[30] = new LandTile("Cracklaw Point", 31, "None");
+        vertices[31] = new SeaTile("Shipbreaker Bay", 32, "None");
+        vertices[32] = new LandTile("Searoad Marches", 33, "None");
+        vertices[33] = new LandTile("Blackwater", 34, "None");
+        vertices[34] = new LandTile("King's Landing", 35, "None");
+        vertices[35] = new SeaTile("Blackwater Bay", 36, "None");
+        vertices[36] = new LandTile("Dragonstone", 37, "Baratheon");
+        vertices[37] = new PortTile("Dragonstone Port", 38, "Baratheon");
+        vertices[38] = new SeaTile("West Summer Sea", 39, "None");
+        vertices[39] = new SeaTile("Redwyne Straights", 40, "None");
+        vertices[40] = new LandTile("Highgarden", 41, "Tyrel");
+        vertices[41] = new LandTile("The Reach", 42, "None");
+        vertices[42] = new LandTile("Kingswood", 43, "None");
+        vertices[43] = new LandTile("Oldtown", 44, "None");
+        vertices[44] = new PortTile("Oldtown Port", 45, "None");
+        vertices[45] = new LandTile("Dornish Marches", 46, "None");
+        vertices[46] = new LandTile("The Boneway", 47, "None");
+        vertices[47] = new LandTile("Storms End", 48, "None");
+        vertices[48] = new PortTile("Storms End Port", 49, "None");
+        vertices[49] = new LandTile("Three Towers", 50, "None");
+        vertices[50] = new LandTile("Prince's Pass", 51, "None");
+        vertices[51] = new SeaTile("Sea of Dorne", 52, "None");
+        vertices[52] = new LandTile("The Arbor", 53, "None");
+        vertices[53] = new LandTile("Starfall", 54, "None");
+        vertices[54] = new LandTile("Yronwood", 55, "None");
+        vertices[55] = new LandTile("Sunspear", 55, "Dorne");
+        vertices[56] = new PortTile("Sunspear Port", 56, "Dorne");
+        vertices[56] = new LandTile("Salt Shore", 57, "None");
+        vertices[57] = new SeaTile("East Summer Sea", 58, "None");
+        
         
         for(int i = 0; i < SIZE; i++){
             graph.addVertex(vertices[i], true);
